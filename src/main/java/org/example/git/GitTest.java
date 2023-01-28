0acd2ac5773fd0619ac3af76f1824594416d6f7f
clare <clare@cc.com>
2023-01-28 19:56:33 +0900

    first commit

---
+++ src/main/java/org/example/git/GitTest.java
@@ -0,0 +1,12 @@
+package org.example.git;
+
+/**
+ * @author wycla
+ * @description TODO test
+ * @create 2023-01-28 19:51
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
